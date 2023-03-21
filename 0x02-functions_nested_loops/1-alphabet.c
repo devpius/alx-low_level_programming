16b4f8f21723fc211e5fc0417f093ff18630ee7e
Gabula Pius <mcduell24@gmail.com>
2023-03-21 20:30:59 +0300

    function to print alphabet in lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+/*
+ * Author: Gabula Pius << devpius >>
+ * Email: mcduell24@gmail.com
+ * Date: 21st-March-2023
+ */
+
+#include"main.h"
+
+/**
+ * print_alphabet - prints the aplhabet in lowercase, 
+ * 		    followed by a new line.
+ */
+void print_alphabet(void)
+{
+	char c;
+	for (c = 'a'; c <= 'z'; ++c)
+		_putchar(c);
+
+	_putchar('\n');
+}
+
