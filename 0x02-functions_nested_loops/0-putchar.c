8653c76a473b054b09430148137b30128c47cf35
Gabula Pius <mcduell24@gmail.com>
2023-03-21 19:17:06 +0300

    program prints '_putchar' followed by a newline

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+/*
+ * Author: Gabula Pius << devpius >>
+ * Email: mcduell24@gmail.com
+ * Date: 21st March 2023
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: the program prints "_putchar"
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	puts("_putchar");
+	return (0);
+}
+
