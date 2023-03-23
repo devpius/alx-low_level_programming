51a2322b92ac92ffab2f32a82f8568073a1eb945
Gabula Pius <mcduell24@gmail.com>
2023-03-24 00:45:40 +0300

    commenting out infinite loop code

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,29 @@
+/*
+ * Author: Gabula Pius << devpius >>
+ * Email: mcduell24@gmail.com
+ * Date: 23rd-March-2023
+ */
+
+#include <stdio.h>
+
+/**
+* main - causes an infinite loop
+* Return: 0
+*/
+
+int main(void)
+{
+	int i;
+
+	printf("Infinite loop incoming :(\n");
+
+	i = 0;
+
+	/*  while (i < 10) */
+	/* { */
+	/*	putchar(i); */
+	/* } */
+
+	printf("Infinite loop avoided! \\o/\n");
+	return (0);
+}
