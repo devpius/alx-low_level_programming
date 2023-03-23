08a2255344061fb03db21596309c9d5163aefdbc
Gabula Pius <mcduell24@gmail.com>
2023-03-24 02:43:00 +0300

    function that prints numbers 0-9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,22 @@
+/*
+ * Author: Gabula Pius << devpius >>
+ * Email: mcduell24@gmail.com
+ * Date: 23rd-March-2023
+ */
+
+/**
+  * print_numbers - prints numbers from 0 to 9
+  * Return: void
+  */
+
+void print_numbers(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+	_putchar(c);
+	}
+
+	_putchar('\n');
+}
