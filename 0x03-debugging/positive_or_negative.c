73f2388eb55672a68d40d0bd67f5dc47f894260a
Gabula Pius <mcduell24@gmail.com>
2023-03-24 00:34:33 +0300

    debugging postive or negative with all errors fixed

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+
+/**
+ * postive_or_negative - function to check for postive or negative number
+ * @i: takes parameter i of type int
+ */
+
+void positive_or_negative(int i)
+{
+	if (i > 0)
+		printf("%i is positive\n", i);
+	else if (i < 0)
+		printf("%i is negative\n", i);
+	else
+		printf("%i is zero\n", i);
+}
