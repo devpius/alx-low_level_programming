7e89b1eb9005e701f07898d87be6f76aa2726712
Gabula Pius <mcduell24@gmail.com>
2023-03-24 05:48:32 +0300

    print numbers

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,35 @@
+/*
+ * Author: Gabula Pius << devpius >>
+ * Email: mcduell24@gmail.com
+ * Date: 23rd-March-2023
+ */
+
+#include "main.h"
+/**
+ * print_number - prints an integer
+ * @n:.input integer parameter
+ *
+ *Owned by Bwave ICT/ Bright Daniel
+ */
+
+#include "main.h"
+/**
+ * print_number - prints an integer
+ * @n:.input integer parameter
+ */
+void print_number(int n)
+{
+	unsigned int i = n;
+
+	if (n < 0)
+	{
+		_putchar(45);
+		i = -i;
+	}
+	if (i / 10)
+	{
+		print_number(i / 10);
+	}
+	_putchar(i % 10 + '0');
+}
+
