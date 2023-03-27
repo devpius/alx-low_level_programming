f5d3880c0443c52e25aa978897fc169345d75c83
Gabula Pius <mcduell24@gmail.com>
2023-03-28 00:21:16 +0300

    reset the pointer to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+*  reset_to_98 - takes the pointer and updates the value
+*  @n: paramter of type int to be checked
+*  Return: 0 is success
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
