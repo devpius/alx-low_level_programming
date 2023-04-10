7f605e112a4c763ac837bfb04901b819d72f07c7
Gabula Pius <mcduell24@gmail.com>
2023-04-10 22:35:07 +0300

    program to print its own name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,21 @@
+/*
+ * Author: Gabula Pius <github.com/devpius>
+ * Email: mcduell24@gmail.com
+ * Date: Mon, 10 Apr 2023
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: program that prints its name
+ * @argc: size of argv(arguments count)
+ * @argv: array of arguments
+ * Return: Always 0 (Success)
+*/
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
+
