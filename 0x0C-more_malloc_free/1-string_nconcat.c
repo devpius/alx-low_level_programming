c064c62d14834e1855a309c8d1e46066403b427c
Gabula Pius <mcduell24@gmail.com>
2023-04-14 00:11:07 +0300

    fucntion that concatenantes string

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,48 @@
+/*
+ * Author: Gabula Pius <github.com/devpius>
+ * Email: mcduell24@gmail.com
+ * Date: Fri, 14 Apr 2023
+ */
+
+#include <stdio.h>
+
+/**
+ * string_nconcat - concatenates two strings
+ * Return: str or NULL if it fails
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int i, j, k;
+	char *str;
+
+	if (s1 == NULL)
+	{
+		i = 0;
+	}
+	else
+	{
+		for (i = 0; s1[i]; ++i)
+		;
+	}
+	if (s2 == NULL)
+	{
+		j = 0;
+	}
+	else
+	{
+		for (j = 0; s2[j]; ++j)
+		;
+	}
+	if (j > n)
+		j = n;
+	str = malloc(sizeof(char) * (i + j + 1));
+	if (str == NULL)
+		return (NULL);
+	for (k = 0; k < i; k++)
+		str[k] = s1[k];
+	for (k = 0; k < j; k++)
+		str[k + i] = s2[k];
+	str[i + j] = '\0';
+	return (str);
+}
+
