625067b1bfe00343c5eda0a3be1e514212e1bbb4
Gabula Pius <mcduell24@gmail.com>
2023-04-18 23:30:50 +0300

    define struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -3,13 +3,13 @@
 
 /**
  * struct dog - dog blueprint
- * 
+ *
  * @name: name of the dog
  * @age: age of the dog
  * @owner: owner of the dog
  */
 
-struct DOG
+struct dog
 {
 	char *name;
 	float age;
