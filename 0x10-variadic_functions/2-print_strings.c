132b533ab532182b22c2152427f7bcf521d7ea04
Gabula Pius <mcduell24@gmail.com>
2023-04-20 19:31:25 +0300

    function that prints numbers.

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -25,6 +25,11 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		s = va_arg(args, char*);
+		if (s == NULL)
+		{
+			s = "(nil)";
+		}
+
 		printf("%s", s);
 		if (i < n - 1)
 			printf("%s", separator);
