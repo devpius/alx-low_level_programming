73969af1f0ab6258fa476b5cc88091f088945e2e
Gabula Pius <mcduell24@gmail.com>
2023-04-20 18:28:25 +0300

    function that prints sum of numbers

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,35 @@
+/*
+ * Author: Gabula Pius <github.com/devpius>
+ * Email: mcduell24@gmail.com
+ * Date: Thu, 20 Apr 2023
+ */
+
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+ * sum_them_all - variadic function to sum all it arguments
+ * @n: number of arguments
+ * Return: Always 0 if args are null,
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	
+	if (n == 0)
+		return (0);
+	
+
+	int i, sum = 0;
+
+	va_list args;
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(args, int);
+	}
+
+	va_end(args);
+	return (sum);
+}
