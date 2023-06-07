f9653028e2bd6d2e665b785bd56033f8eb281660
Pius Gabula <mcduell24@gmail.com>
2023-06-07 13:29:17 +0300

    function that prints a string, followed by a new line

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,24 @@
+/*
+ * Author: Gabula Pius <github.com/devpius>
+ * Email: mcduell24@gmail.com
+ */
+
+#include "main.h"
+
+/**
+* _puts_recursion - prints a string
+* @s: parameter to be printed
+* Return: void
+*/
+void _puts_recursion(char *s)
+{
+    if(*s)
+    {
+        _putchar(*s);
+        _puts_recursion(s + 1);
+    }
+    else 
+    {
+        printf(" ");
+    }
+}
