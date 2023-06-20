a1bfbdcf351158508ac2412aea475d922bca1738
Pius Gabula <mcduell24@gmail.com>
2023-06-20 21:54:19 +0300

    fix main

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,18 @@
+/*
+ * Author: Gabula Pius <github.com/devpius>
+ * Email: mcduell24@gmail.com
+ * Date: Tue, 18 Apr 2023
+ */
+
+#include <stdio.h>
+
+/**
+ * main - prints the name of the file it was compiled with
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
+
